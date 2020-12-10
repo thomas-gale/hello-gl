49815b082000ee3133bb825f4261ef3243873af0
Thomas Gale <gale.thomasjames@gmail.com>
2020-12-10 21:39:24 +0000

    Opening glfw window.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hello-gl
 Following https://learnopengl.com/
+
+# [Development Setup](https://gist.github.com/thomas-gale/70987288d4aed1b6e6b9086341a55fa2)
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 
-int main() { std::cout << "Hello GL" << std::endl; }
+#include "GLFW/glfw3.h"
+//#include "glad/glad.h"
+
+int main() {
+    // Init
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "HelloGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    return 0;
+}
