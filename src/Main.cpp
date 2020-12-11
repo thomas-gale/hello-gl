8443a58698beab6dc48663312389db800968c1a9
Thomas Gale <gale.thomasjames@gmail.com>
2020-12-11 12:08:04 +0000

    Passing vertex color into the fragment shader.

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,16 +7,19 @@
 const char* vertexShaderSrc =
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
     "}\0";
 
 const char* fragShaderSrc = "#version 330 core\n"
                             "out vec4 FragColor;\n"
+                            "in vec4 vertexColor;\n"
                             "void main()\n"
                             "{\n"
-                            "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                            "    FragColor = vertexColor;\n"
                             "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -163,7 +166,7 @@ int main() {
         // 4. Draw stuff.
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Draw lines. GL_FILL
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // Draw lines. GL_FILL
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
